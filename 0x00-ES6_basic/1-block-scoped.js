fdc52ef96fd9896ee1323ed54a28f4468424c391
Willmuseve <willmuseve@gmail.com>
2024-03-13 19:11:57 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Changed var to let
-  let task2 = true; // Changed var to let
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Removed var
-    task2 = false; // Removed var
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
