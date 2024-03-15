629f3044b40345e6e05e6b03e18c936a0383a6a3
Willmuseve <willmuseve@gmail.com>
2024-03-15 15:04:52 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
-def getListStudents():
-    students = [
-        {"id": 1, "firstName": "Guillaume", "location": "San Francisco"},
-        {"id": 2, "firstName": "James", "location": "Columbia"},
-        {"id": 5, "firstName": "Serena", "location": "San Francisco"}
-    ]
-    return students
+function getListStudents() {
+    return [
+        { id: 1, firstName: "Guillaume", location: "San Francisco" },
+        { id: 2, firstName: "James", location: "Columbia" },
+        { id: 5, firstName: "Serena", location: "San Francisco" }
+    ];
+}
+
+const students = getListStudents();
+console.log(students);
