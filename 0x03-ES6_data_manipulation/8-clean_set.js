e2e0381be9724ef8a97e069076ac808cace5d435
Willmuseve <willmuseve@gmail.com>
2024-03-15 20:27:51 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+export default function cleanSet(set, startString) {
+  const str = [];
+
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const subString = value.substring(startString.length);
+
+      if (subString && subString !== value) {
+        str.push(subString);
+      }
+    }
+  }
+  return (str.join('-'));
+}
