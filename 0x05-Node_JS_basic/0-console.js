65c0999410ad0cf626cd5445159ff9efbc2534c3
Willmuseve <willmuseve@gmail.com>
2024-04-29 18:55:24 +0000

    basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,8 @@
 // Define a function named displayMessage
 
 function displayMessage(message) {
-    // Print the message to STDOUT
-    console.log(message);
+	return console.log(message);
 }
 
 // Export the displayMessage
-// function to make it accessible from other modules
 module.exports = displayMessage;
